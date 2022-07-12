2cdada341bb066da52c7248dcf8a2c7de132feb2
iyear <ljyngup@gmail.com>
2022-07-12 13:52:57 +0800

    fix(group): media caption index

--- app/bot/internal/handler/group/on_msg.go
+++ app/bot/internal/handler/group/on_msg.go
@@ -13,14 +13,12 @@ import (
 func Index(c tele.Context) error {
 	m := c.Message()
 
-	sp := util.GetScope(c)
-
-	return sp.Storage.Search.Index([]*storage.SearchItem{{
+	return util.GetScope(c).Storage.Search.Index([]*storage.SearchItem{{
 		ID: keygen.SearchMsgID(m.Chat.ID, m.ID),
 		Data: &models.SearchMsg{
 			ID:     strconv.Itoa(m.ID),
 			Chat:   strconv.FormatInt(m.Chat.ID, 10),
-			Text:   m.Text,
+			Text:   c.Text(),
 			Sender: strconv.FormatInt(m.Sender.ID, 10),
 			Date:   strconv.FormatInt(time.Now().Unix(), 10),
 		}},
