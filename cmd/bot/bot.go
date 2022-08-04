3ec5609276f9bfdc89e3e2a2de1ea7ff210ce2b5
iyear <ljyngup@gmail.com>
2022-08-04 19:43:55 +0800

    refactor!(cmd): bot cmd

---
+++ cmd/bot/bot.go
@@ -0,0 +1,17 @@
+package bot
+
+import (
+	"github.com/iyear/searchx/cmd/bot/query"
+	"github.com/iyear/searchx/cmd/bot/run"
+	"github.com/iyear/searchx/cmd/bot/source"
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "bot",
+	Short: "Official telegram bot for group/channel owner",
+}
+
+func init() {
+	Cmd.AddCommand(run.Cmd, query.Cmd, source.Cmd)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,18 @@
 package cmd
 
 import (
-	"github.com/iyear/searchx/cmd/query"
-	"github.com/iyear/searchx/cmd/run"
-	"github.com/iyear/searchx/cmd/source"
+	"github.com/iyear/searchx/cmd/bot"
 	"github.com/iyear/searchx/cmd/version"
 	"github.com/spf13/cobra"
 )
 
-// TODO
 var cmd = &cobra.Command{
 	Use:   "searchx",
 	Short: "Enhance Telegram Group/Channel Search In 5 Minutes",
 }
 
 func init() {
-	cmd.AddCommand(run.Cmd)
-	cmd.AddCommand(source.Cmd)
-	cmd.AddCommand(query.Cmd)
-	cmd.AddCommand(version.Cmd)
+	cmd.AddCommand(bot.Cmd, version.Cmd)
 }
 
 func Execute() {
