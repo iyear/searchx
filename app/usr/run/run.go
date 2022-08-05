ff3db738d7690ac1db6b247ec2a036716e55d90d
iyear <ljyngup@gmail.com>
2022-08-05 22:30:53 +0800

    feat(usr): run init

--- app/usr/run/run.go
+++ app/usr/run/run.go
@@ -1,10 +1,26 @@
 package run
 
+import (
+	"github.com/fatih/color"
+	"github.com/iyear/searchx/app/usr/run/internal/config"
+	"github.com/iyear/searchx/global"
+	"log"
+)
+
 func Run(cfg string, login bool) {
+	color.Blue(global.Logo)
+	color.Blue("Initializing...")
+
+	if err := config.Init(cfg); err != nil {
+		log.Fatalf("init config failed: %v", err)
+	}
+	color.Blue("Config loaded")
 
 	if login {
 		startLogin()
 		return
 	}
+
+	// slog := logger.New(config.C.Ctrl.Log.Enable, "log/usr/latest.log", config.C.Ctrl.Log.Level)
 
 }
